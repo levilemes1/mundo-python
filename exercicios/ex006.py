48c2797d6ac049eab0fea02b32119bdf0ffebcac
Levi Leme <levilemes1@gmail.com>
2022-06-03 20:34:51 -0300

    treinando python

---
+++ exercicios/ex006.py
@@ -0,0 +1,3 @@
+n = int(input('Digite um número: '))
+print(f'O dobro de {n} vale {n * 2}')
+print(f'O triplo de {n} vale {n * 3} \nA raiz quadrada de {n} é igual a {pow(n, (1 / 2))}')
---
+++ exercicios/ex010.py
@@ -0,0 +1,3 @@
+real = float(input('Quanto dinheiro você tem na carteira? R$'))
+dolar = real / 4.77
+print(f'Com R${real} você pode comprar US${dolar}')
