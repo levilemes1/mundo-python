ed27d3ed844d829e951f3d5eb74f8d5d07b470fc
Levi Leme <levilemes1@gmail.com>
2022-06-04 21:41:46 -0300

    exercicios da aula de python

---
+++ exercicios/ex018.py
@@ -0,0 +1,9 @@
+from math import radians, sin, cos, tan
+
+angulo = float(input('Digite o ângulo que você deseja: '))
+seno = sin(radians((angulo)))
+print(f'O ângulo de {angulo} tem o SENO de {seno}')
+cosseno = cos(radians(angulo))
+print(f'O ângulo de {angulo} tem o COSSENO de {cosseno}')
+tangente = tan(radians(angulo))
+print(f'O ângulo de {angulo} tem o SENO de {tangente}')
